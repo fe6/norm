2a1aad1c1eb928d29c6512042a1cb0f4f7f0a713
undefined <undefined>
2021-06-17 09:50:41 +0800

    feat($vue3): add vue 3 eslint

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const prettier = require('./src/prettier');
 const stylelint = require('./src/stylelint');
-const jslint = require('./src/js');
-const tslint = require('./src/ts');
-const jestlint = require('./src/jest');
-const vue2Jslint = require('./src/vue2-js');
-const vue2Tslint = require('./src/vue2-ts');
+const jslint = require('./src/eslint/js');
+const tslint = require('./src/eslint/ts');
+const jestlint = require('./src/eslint/jest');
+const vue2Jslint = require('./src/eslint/vue2-js');
+const vue2Tslint = require('./src/eslint/vue2-ts');
+const vue3Tslint = require('./src/eslint/vue3-ts');
 
 module.exports = {
   prettier,
@@ -15,4 +16,5 @@ module.exports = {
   jestlint,
   vue2Jslint,
   vue2Tslint,
+  vue3Tslint,
 };
---
+++ src/eslint/vue3-ts.js
@@ -0,0 +1,63 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: '@antfu/eslint-config',
+  plugins: [
+    'markdown',
+  ],
+  rules: {
+    'no-alert': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/consistent-function-scoping': 'off',
+    'vue/experimental-script-setup-vars': 2,
+    'unicorn/no-null': 'off',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          kebabCase: true,
+        },
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/no-string-refs': 'off',
+    'no-new': 'off',
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/order': 'off',
+    'react/no-unknown-property': 'off',
+    'vue/no-deprecated-functional-template': 'off',
+    'vue/one-component-per-file': 'off',
+    'spaced-comment': ['error', 'always', { exceptions: ['@__PURE__'] }],
+  },
+  overrides: [
+    {
+      files: ['*.md', '*.vue', '*.ts', '*.tsx', 'scripts/*.ts'],
+      rules: {
+        'no-console': 'warn',
+        'no-undef': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ['*.vue'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              pascalCase: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
